cffe4e47fca1494bdff50d31f4cd98464f3248a9
bb-frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2026-02-19 12:14:56 -0500

    feat: add Tailwind CSS and related dependencies to enhance styling capabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import UnoCSS from '@unocss/astro';
 import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    UnoCSS({
-      injectReset: true // or a path to the reset file
-    }),
     react()
-  ]
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
