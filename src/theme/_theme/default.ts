3aa2f2825d59e28001df5047306bacbbe91f918f
bb-frontend-leader <coor_frontend@booksandbooksdigital.com.co>
2026-02-19 12:15:43 -0500

    chore: remove unused uno.config.ts file

--- src/theme/_theme/default.ts
+++
@@ -1,8 +0,0 @@
-import type { Theme } from '../types/types';
-
-import { preflightBase, preflightRoot } from './preflight';
-
-export const theme = {
-  preflightRoot,
-  preflightBase
-} satisfies Theme;
--- uno.config.ts
+++
@@ -1,7 +0,0 @@
-import { presetOVA } from './src/theme/preset-ova'
-
-import { defineConfig } from 'unocss';
-
-export default defineConfig({
-  presets: [presetOVA()]
-});
