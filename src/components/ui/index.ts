3682660876ec657ac879f4c1e34bf19e6eb90f9c
Samara Smith Rincon Montaña <FRONTEND2@holdingbb.col>
2026-02-20 13:03:34 -0500

    feat: implement HeroProgram and ProgramFeatures sections with responsive design and content updates

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 
 export * from './dropdown-menu'
 export * from './button'
 export * from './link'
 export * from './card'
 export * from './hero-carousel'
+=======
+export * from './dropdown-menu';
+export * from './button';
+export * from './link';
+export * from './hero-carousel';
+export * from './message-card';
+>>>>>>> Stashed changes
