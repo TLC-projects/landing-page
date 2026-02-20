a656b1a628b20c4e6e8a685fed8810808ac2be92
bb-frontend-11 <desarrolladorfrontend2@booksandbooksdigital.com.co>
2026-02-20 14:41:04 -0500

    fix: remove duplicate export of HeroCarousel component from index file

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,7 +3,6 @@ export * from './dropdown-menu';
 export * from './button';
 export * from './link';
 export * from './card';
-export * from './hero-carousel';
 export * from './dropdown-menu'
 export * from './button'
 export * from './link'
