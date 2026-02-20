c9ad023f6ca532142fd454a1ad1c17c2e118f5aa
bb-frontend-11 <desarrolladorfrontend2@booksandbooksdigital.com.co>
2026-02-20 14:36:43 -0500

    feat: add Banner, CoursesSection, and ImageListSection components with responsive design and content integration for the page "capacitacion-docente"

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,11 +1,8 @@
 
-export * from './dropdown-menu'
-export * from './button'
-export * from './link'
-export * from './card'
-export * from './hero-carousel'
 export * from './dropdown-menu';
 export * from './button';
 export * from './link';
+export * from './card';
 export * from './hero-carousel';
 export * from './message-card';
+export * from './course-card';
